5f416768fd90037bbfb27a3cd7eb77418bbd838a
Jacob Cassidy <jacob@jacobcassidy.com>
2021-11-27 18:53:44 +0700

    Update answer-comparison.js to v3

--- type-match/answer-comparison.js
+++ type-match/answer-comparison.js
@@ -1,72 +1,104 @@
-window.addEventListener('DOMContentLoaded', (event) => {
-  compareAnswers();
-});
+const typedAnswerEl = document.getElementById('typedAnswer'),
+      typedAnswerText = typedAnswerEl.textContent,
+      typedAnswerCharArr = typedAnswerText.split(''),
+      cardAnswerEl = document.getElementById('cardAnswer'),
+      cardAnswerText = cardAnswerEl.textContent,
+      cardAnswerCharArr = cardAnswerText.split(''),
+      dmp = new diff_match_patch(),
+      typedDiff = dmp.diff_main(typedAnswerText, cardAnswerText),
+      cardDiff = dmp.diff_main(cardAnswerText, typedAnswerText);
 
-function compareAnswers() {
-  const typedAnswerEl = document.getElementById('typedAnswer');
-  const typedAnswerText = typedAnswerEl.textContent;
-  const typedAnswerCharArr = typedAnswerText.split('');
-  const cardAnswerEl = document.getElementById('cardAnswer');
-  const cardAnswerText = cardAnswerEl.textContent;
-  // const cardAnswerCharArr = cardAnswerText.split('');
+let   typedMatchCharArr = [],
+      typedAnswerComparisonArr = [],
+      cardMatchCharArr = [],
+      cardAnswerComparisonArr = [];
 
-  // let typedMatchArr = [];
-  let typedAnswerComparisonArr = [];
-  // let typedAnswerComparisonHTML = '';
-  // let cardAnswerComparisonHTML = '';
-  let lastCardMatchIndex = -1;
-  // let cardMatchIndexArr = [];
+function addToTypedMatchCharArr() {
+  for (let i = 0; i < typedDiff.length; i++ ) {
+    let typedStr = typedDiff[i][1]; // 'plus'
+    let typedStrArr = typedStr.split(''); // ['p', 'l', 'u', 's']
+    let isTypedMatch = typedDiff[i][0]; // -1
 
+    typedStrArr.forEach( char => {
+      let typedCharDiff = [isTypedMatch, char]; // [-1, 'p']
+      if ( isTypedMatch !== 1 ) {
+        typedMatchCharArr.push(typedCharDiff);
+      }
+    });
+  }
+}
 
-  // For each typedAnswerCharArr index, find the next available match in cardAnswerCharArr
-  for (let i = 0; i < typedAnswerCharArr.length; i++ ) {
+function addToCardMatchCharArr() {
+  for ( let i = 0; i < cardDiff.length; i++ ) {
+    let cardStr = cardDiff[i][1]; // '+,' string
+    let cardStrArr = cardStr.split(''); // ['+', ',']
+    let isCardMatch = cardDiff[i][0]; // -1, 0, or 1
 
-    let typedChar = typedAnswerCharArr[i];
-    let typedCharEscaped = typedChar.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // $& means the whole matched string
-    let regexp = new RegExp(typedCharEscaped, 'g');
-    let cardToTypedMatchArr = Array.from(cardAnswerText.matchAll(regexp), match => match['index']);
-    let wrapTypedChar;
+    cardStrArr.forEach( char => {
+      let cardCharDiff = [isCardMatch, char]; // [0, 'K']
+      if ( isCardMatch !== 1 ) {
+        cardMatchCharArr.push(cardCharDiff);
+      }
+    });
+  }
+}
 
-    console.log('\nlast card match: ' + lastCardMatchIndex);
-    console.log('card match array: ' + cardToTypedMatchArr);
+function addToTypedAnswerComparisonArr() {
+  let lastCorrectCardCharIndex = -1;
+  let lastCorrectTypedCharIndex = -1;
+  let dashesInserted = 0;
 
-    if ( cardToTypedMatchArr.length !== 0  ) {
+  for( let i = 0; i < typedMatchCharArr.length; i++ ) {
+    let typedMatchNum = typedMatchCharArr[i][0];
+    let typedMatchChar = typedMatchCharArr[i][1];
+    let wrapTypedMatchChar;
 
-      let cardMatchIndex = cardToTypedMatchArr.find( match => match > lastCardMatchIndex );
+    if ( typedMatchNum === -1 ) {
+      wrapTypedMatchChar = '<span class="typedIncorrect">' + typedMatchChar + '<span>';
 
-      console.log('available match index: ' + cardMatchIndex);
+    } else if ( typedMatchNum === 0 ) {
+      let cardMatchCharIndex = cardMatchCharArr.findIndex( (innerArr, index) => innerArr[0] === 0 && innerArr[1] === typedMatchChar && index > lastCorrectCardCharIndex );
+      let indexWithDashes = i + dashesInserted;
 
-      if ( cardMatchIndex <= typedAnswerComparisonArr.length )  {
-        wrapTypedChar = '<span class="typedCorrect">' + typedChar + '</span>';
-        lastCardMatchIndex  = cardMatchIndex;
-        // cardMatchIndexArr.push( lastCardMatchIndex );
-
-      } else if ( cardMatchIndex ) {
-        let dashesNeeded =  cardMatchIndex - typedAnswerComparisonArr.length;
-        let dashesString =  '<span class="typedIncorrect">-</span>';
+      if ( cardMatchCharIndex > indexWithDashes ) {
+        let dashesNeeded = cardMatchCharIndex - indexWithDashes;
+        let dashesStr = '<span class="typedIncorrect">-</span>';
         let dashesAdded = 0;
 
         while ( dashesNeeded > dashesAdded ) {
-          typedAnswerComparisonArr.splice( lastCardMatchIndex + 1, 0, dashesString );
+          typedAnswerComparisonArr.splice( lastCorrectTypedCharIndex + 1, 0, dashesStr );
           dashesAdded++;
         }
 
-        wrapTypedChar = '<span class="typedCorrect">' + typedChar + '</span>';
-        // cardMatchIndexArr.push(cardMatchIndex);
-        lastCardMatchIndex  = cardMatchIndex;
-        // typedMatchArr.push(i);
-
-      } else {
-        wrapTypedChar = '<span class="typedIncorrect">' + typedChar + '</span>';
+        dashesInserted += dashesNeeded;
       }
-
-    } else {
-      wrapTypedChar = '<span class="typedIncorrect">' + typedChar + '</span>';
+      wrapTypedMatchChar = '<span class="typedCorrect">' + typedMatchChar + '</span>';
+      lastCorrectCardCharIndex = cardMatchCharIndex;
+      lastCorrectTypedCharIndex = indexWithDashes;
     }
-
-    typedAnswerComparisonArr.push(wrapTypedChar);
+    typedAnswerComparisonArr.push(wrapTypedMatchChar);
   }
-
-  typedAnswerEl.innerHTML = typedAnswerComparisonArr.join('');
-  // cardAnswerEl.innerHTML = cardAnswerComparisonHTML;
 }
+
+function addToCardAnswerComparisonArr() {
+  for( let i = 0; i < cardMatchCharArr.length; i++ ) {
+    let cardMatchNum = cardMatchCharArr[i][0];
+    let cardMatchChar = cardMatchCharArr[i][1];
+
+    if ( cardMatchNum === -1 ) {
+      let wrapCardMatchChar = '<span class="typedNotFound">' + cardMatchChar + '<span>';
+      cardAnswerComparisonArr.push(wrapCardMatchChar);
+
+    } else if ( cardMatchNum === 0 ) {
+      let wrapCardMatchChar = '<span class="typedCorrect">' + cardMatchChar + '<span>';
+      cardAnswerComparisonArr.push(wrapCardMatchChar);
+    }
+  }
+}
+
+addToTypedMatchCharArr();
+addToCardMatchCharArr();
+addToTypedAnswerComparisonArr();
+addToCardAnswerComparisonArr();
+typedAnswerEl.innerHTML = typedAnswerComparisonArr.join('');
+cardAnswerEl.innerHTML = cardAnswerComparisonArr.join('');
