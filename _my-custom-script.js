49fd466f348b1b1d134d1ebfddea9cc3911ea2ac
Jacob Cassidy <jacob@jacobcassidy.com>
2021-11-30 03:49:35 +0700

    Refactor JavaScript

--- _my-custom-script.js
+++ _my-custom-script.js
@@ -1,158 +1,207 @@
-var hasMyCustomScript = true,
-    isAnkiPc21 = typeof pycmd !== 'undefined',
-    defaultInput,
-    newTextarea,
-    textareaAnswer,
-    dataInput,
-    dataOutput,
-    needsOutput,
-    hasBonus,
-    bonusInputWrap,
-    bonusOutputWrap,
-    notesWrap,
-    hasNote;
+const hasMyCustomScript = true,
+      isAnkiPC21 = typeof pycmd !== 'undefined';
 
-createTextarea();
-printTextareaAnswer();
-focusTextarea();
-activateBonus();
-toggleBonusQuestion()
-persistData();
-printPersistentData();
-printNote();
+let   dataOutputArr;
+
 watchQA();
 
 function watchQA() {
-  // Watch for changes in div#qa and run functions when change
+  // Run once on first load
+  showInput();
+  focusInput();
+  showPrimaryTitle();
+  modifyNotes();
+  holdData();
+  showOutput();
+
+  // Then watch for changes in div#qa and run functions again when div#qa changes
   const targetNode = document.getElementById('qa');
   const config = { childList: true };
-  const callback = function(mutationsList, observer) {
-    for (const mutation of mutationsList) {
-      if (mutation.type === 'childList') {
-        createTextarea();
-        printTextareaAnswer();
-        focusTextarea();
-        activateBonus();
-        toggleBonusQuestion()
-        persistData();
-        printPersistentData();
-        printNote();
+  const callback = function( mutationsList, observer ) {
+    for ( const mutation of mutationsList ) {
+      if ( mutation.type === 'childList' ) {
+        showInput();
+        focusInput();
+        showPrimaryTitle();
+        modifyNotes();
+        holdData();
+        showOutput();
       }
-      break;
+      break; // don't loop again from changes made to div#qa by running this script
     }
   };
-  const observer = new MutationObserver(callback);
-  observer.observe(targetNode, config);
+  const observer = new MutationObserver( callback );
+  observer.observe( targetNode, config );
 }
 
-function createTextarea() {
-  // Changes Anki's default input field into a textarea on desktop
-  defaultInput = document.getElementById('typeans');
-  if (defaultInput && defaultInput.nodeName == "INPUT") {
-    newTextarea = document.createElement('textarea');
-    newTextarea.id = defaultInput.id;
-    newTextarea.autofocus = true;
-    defaultInput.parentNode.replaceChild(newTextarea, defaultInput);
-    typeans = newTextarea;
-    if (isAnkiPc21) {
-      newTextarea.addEventListener('keydown', event => {
-        if (event.ctrlKey && event.key == 'Enter') pycmd("ans");
-      });
-    }
-  }
-}
+function showInput() {
+  let inputWrapList = document.querySelectorAll('.input-wrap');
+  if ( inputWrapList.length !== 0 ) {
+    inputWrapList.forEach( inputWrap => {
+      let placeholder = inputWrap.querySelector('.faux-placeholder');
 
-function printTextareaAnswer() {
-  // Replace default output with trimmed HTML
-  textareaAnswer = document.getElementById('textarea-output');
-  if (textareaAnswer) {
-    typeans = document.getElementById('typeans');
-    let code = document.createElement('code');
-    code.setAttribute('id','typeans');
-    code.innerHTML = typeans.innerHTML.trim();
-    textareaAnswer.innerHTML = '';
-    textareaAnswer.appendChild(code);
-  }
-}
+      // If faux placeholder question is not empty, show input
+      if ( placeholder !== null && placeholder.innerHTML !== '' ) {
+        let inputTitle = inputWrap.querySelector('.input-title');
+        let textarea = inputWrap.querySelector('textarea');
+        inputWrap.classList.add('active');
 
-function focusTextarea() {
-  // If textarea#typeans does not exist, add focus to new textarea element
-  let textarea = document.querySelector('textarea');
-  if (textarea && textarea.id !== 'typeans') {
-    textarea.focus();
-  }
-}
+        if ( inputTitle !== null ) {
+          let hasCompare = inputTitle.getAttribute('data-compare');
+          let hasTypeHint = inputTitle.getAttribute('data-type-hint');
 
-function activateBonus() {
-  // Activate front card bonus box if bonus question exists
-  bonusInputWrap = document.getElementById('bonus-input-wrap');
-  hasBonus = document.getElementById('hasBonus');
-  if (bonusInputWrap && hasBonus.innerHTML) {
-    bonusInputWrap.classList.add("active");
-  }
-}
+          // Add "precise" text to input title if comparison is active
+          if ( hasCompare && hasCompare !== '' ) {
+            inputTitle.innerHTML += '<span class="is-comparison">precise</span>';
+          }
 
-function toggleBonusQuestion() {
-  // Hide bonus question when typing into bonus textarea
-  let textarea = document.querySelector('#bonus-input-wrap textarea');
-  let bonusQuestion = document.getElementById('hasBonus');
-  if (textarea) {
-    textarea.addEventListener('input', event => {
-      if (textarea.value !== '') {
-        bonusQuestion.classList.add('hidden');
-      } else {
-        bonusQuestion.classList.remove('hidden');
-      }
-    });
-  }
-}
+          // Add type hint text to input title if available
+          if ( hasTypeHint && hasTypeHint !== '' ) {
+            inputTitle.innerHTML += `<span class="is-type-hint">${hasTypeHint}</span>`;
+          }
+        }
 
-function persistData() {
-  dataInput = document.getElementById('data-input');
-  if (dataInput) {
-    // Persist input data of new textarea for AnkiDroid, AnkiMobile, AnkiWeb
-    dataInput.addEventListener('input', (event) => {
-      dataOutput = event.currentTarget.value;
-      if (!isAnkiPc21) {
-        try {
-          sessionStorage.setItem('dataOutput', JSON.stringify(dataOutput));
-        } catch (error) {
-          console.log(`${error.name}: ${error.message}`);
+        // Hide faux placeholder when inputting data
+        if ( textarea !== null ) {
+          textarea.addEventListener( 'input', event => {
+            if ( textarea.value !== '' ) {
+              placeholder.classList.add('inactive');
+            } else {
+              placeholder.classList.remove('inactive');
+            }
+          });
         }
       }
     });
-    // Return input data on desktop
-    if (isAnkiPc21) {
-      dataInput.addEventListener('keydown', event => {
-        if (event.ctrlKey && event.key == 'Enter') pycmd("ans");
-      });
+  }
+}
+
+function focusInput() {
+  // Add focus to first input/textarea element if the ID is not #typeans
+  let inputField = document.querySelector( 'input, textarea' );
+  if ( inputField !== null && inputField.id !== typeans ) {
+    inputField.focus();
+  }
+}
+
+function showPrimaryTitle() {
+  let primary = document.querySelector('.is-primary');
+  if ( primary !== null ) {
+    let textarea = primary.querySelector('textarea');
+    let title = primary.querySelector('.input-title');
+    if ( title !== null ) {
+      let hasCompare = title.getAttribute('data-compare');
+      // Show primary input title if comparison is not active
+      if ( hasCompare && hasCompare === '' ) {
+        title.classList.add('active');
+        textarea.classList.add('has-title');
+      }
     }
   }
 }
 
-function printPersistentData() {
-  // Activate back card bonus box if data will be printed to it
-  bonusOutputWrap = document.getElementById('bonus-output-wrap');
-  if (bonusOutputWrap && dataOutput) {
-    bonusOutputWrap.classList.add("active");
-  }
-  // Print data to back card output box
-  needsOutput = document.getElementById('data-output');
-  if (needsOutput) {
-    needsOutput.textContent = dataOutput;
-    dataOutput = '';
-  }
-}
-
-function printNote() {
-  // Hide notes section if there are no notes added
-  notesWrap = document.getElementById('notes-wrap');
-  hasNote = document.getElementsByClassName('notes')[0];
-  if (notesWrap && !hasNote.innerHTML) {
-    notesWrap.classList.add("inactive");
+function modifyNotes() {
+  // Hide notes box if there is no note content
+  let notesBox = document.querySelector('.notes-box');
+  let hasNote = document.querySelector('.notes')
+  if ( notesBox != null && !hasNote.innerHTML ) {
+    notesBox.classList.add('inactive');
   }
   // Align text left if there are more than 200 characters in the notes
-  if (hasNote && hasNote.innerText.length > 200) {
+  if ( hasNote !== null && hasNote.innerText.length > 200 ) {
     hasNote.classList.add('long');
   }
 }
+
+function holdData() {
+  let inputDataList = document.querySelectorAll('.input-data');
+
+  if ( inputDataList.length !== 0 ) {
+    dataOutputArr = Array(inputDataList.length);
+    inputDataList.forEach( ( inputData, inputIndex ) => {
+      // Insert input data in array for output
+      inputData.addEventListener('input', event => {
+        let inputValue = event.currentTarget.value;
+        dataOutputArr.splice( inputIndex, 1, inputValue );
+      });
+      // Return data on PC keypress
+      if ( isAnkiPC21 ) {
+        inputData.addEventListener('keydown', event => {
+          if (event.ctrlKey && event.key == 'Enter') pycmd("ans");
+        });
+      }
+    });
+  }
+}
+
+function showOutput() {
+  let outputDataList = document.querySelectorAll('.output-data');
+  let outputCompareList = document.querySelectorAll('.output-compare');
+
+  if ( outputDataList.length !== 0 ) {
+    outputDataList.forEach( (outputData, outputIndex) => {
+      outputData.textContent = dataOutputArr[outputIndex];
+    });
+  }
+
+
+
+  // if ( outputDataList.length !== 0 ) {
+  //   outputCompareList.forEach( ( outputCompare, outputIndex ) => {
+  //     let hasCompare = outputCompare.getAttribute('data-compare');
+  //     if ( hasCompare !== '' ) {
+
+  //       outputCompare.innerText = dataOutputArr[outputIndex];
+
+  //     }
+  //   });
+  // } else if ( )
+}
+
+// HIDE .output-compare and .box by default
+// SHOW either .output-compare or .box depending on if compare is active
+// If compare active, insert comparison data
+// Check if it works on AnkiWeb and AnkiMobile iOS
+
+
+// function showInput() {
+//   let inputWrapList = document.querySelectorAll('.input-wrap');
+//   if ( inputWrapList.length !== 0 ) {
+//     inputWrapList.forEach( inputWrap => {
+//       let placeholder = inputWrap.querySelector('.faux-placeholder');
+
+//       // If faux placeholder question is not empty, show input
+//       if ( placeholder !== null && placeholder.innerHTML !== '' ) {
+//         let inputTitle = inputWrap.querySelector('.input-title');
+//         let textarea = inputWrap.querySelector('textarea');
+//         inputWrap.classList.add('active');
+
+//         if ( inputTitle !== null ) {
+//           let hasCompare = inputTitle.getAttribute('data-compare');
+//           let hasTypeHint = inputTitle.getAttribute('data-type-hint');
+
+//           // Add "precise" text to input title if comparison is active
+//           if ( hasCompare && hasCompare !== '' ) {
+//             inputTitle.innerHTML += '<span class="is-comparison">precise</span>';
+//           }
+
+//           // Add type hint text to input title if available
+//           if ( hasTypeHint && hasTypeHint !== '' ) {
+//             inputTitle.innerHTML += `<span class="is-type-hint">${hasTypeHint}</span>`;
+//           }
+//         }
+
+//         // Hide faux placeholder when inputting data
+//         if ( textarea !== null ) {
+//           textarea.addEventListener( 'input', event => {
+//             if ( textarea.value !== '' ) {
+//               placeholder.classList.add('inactive');
+//             } else {
+//               placeholder.classList.remove('inactive');
+//             }
+//           });
+//         }
+//       }
+//     });
+//   }
+// }
